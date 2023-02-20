0a29ac8c1272e8e0a4055456c6b2a51868dc16eb
ChenpoHU <chenpo.charlotte.hu@gmail.com>
2023-02-20 22:31:10 +0100

    prepare the inital image upload, delete function

--- src/app/applyTheme/applyTheme.page.ts
+++ src/app/applyTheme/applyTheme.page.ts
@@ -1,6 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import SwiperCore, { Autoplay, Keyboard, Pagination, Scrollbar, Zoom } from 'swiper';
 import { IonicSlides } from '@ionic/angular';
+import { Filesystem, Directory } from '@capacitor/filesystem';
+import { HttpClient } from '@angular/common/http';
+import { LoadingController, Platform, ToastController } from '@ionic/angular';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+import { finalize } from 'rxjs/operators';
+
+const IMAGE_DIR = 'stored-images';
+
+interface LocalFile {
+	name: string;
+	path: string;
+	data: string;
+}
 
 SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom, IonicSlides]);
 
@@ -9,8 +22,175 @@ SwiperCore.use([Autoplay, Keyboard, Pagination, Scrollbar, Zoom, IonicSlides]);
   templateUrl: 'applyTheme.page.html',
   styleUrls: ['applyTheme.page.scss']
 })
-export class applyThemePage {
+export class applyThemePage implements OnInit {
+	images: LocalFile[] = [];
 
-  constructor() {}
+	constructor(
+		private plt: Platform,
+		private http: HttpClient,
+		private loadingCtrl: LoadingController,
+		private toastCtrl: ToastController
+	) {}
 
+	async ngOnInit() {
+		this.loadFiles();
+	}
+
+	async loadFiles() {
+		this.images = [];
+
+		const loading = await this.loadingCtrl.create({
+			message: 'Loading data...'
+		});
+		await loading.present();
+
+		Filesystem.readdir({
+			path: IMAGE_DIR,
+			directory: Directory.Data
+		})
+			.then(
+				(result) => {
+					this.loadFileData((result.files as any));
+				},
+				async (err) => {
+					// Folder does not yet exists!
+					await Filesystem.mkdir({
+						path: IMAGE_DIR,
+						directory: Directory.Data
+					});
+				}
+			)
+			.then((_) => {
+				loading.dismiss();
+			});
+	}
+
+	// Get the actual base64 data of an image
+	// base on the name of the file
+	async loadFileData(fileNames: string[]) {
+		for (let f of fileNames) {
+			const filePath = `${IMAGE_DIR}/${f}`;
+
+			const readFile = await Filesystem.readFile({
+				path: filePath,
+				directory: Directory.Data
+			});
+
+			this.images.push({
+				name: f,
+				path: filePath,
+				data: `data:image/jpeg;base64,${readFile.data}`
+			});
+		}
+	}
+
+	// Helper
+	async presentToast(text: string) {
+		const toast = await this.toastCtrl.create({
+			message: text,
+			duration: 3000
+		});
+		toast.present();
+	}
+
+  async selectImage() {
+    const image = await Camera.getPhoto({
+        quality: 90,
+        allowEditing: false,
+        resultType: CameraResultType.Uri,
+        source: CameraSource.Photos // Camera, Photos or Prompt
+    });
+
+    if (image) {
+        this.saveImage(image)
+    }
+}
+
+// Create a new file from a capture image
+async saveImage(photo: Photo) {
+    const base64Data = await this.readAsBase64(photo);
+
+    const fileName = new Date().getTime() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+        path: `${IMAGE_DIR}/${fileName}`,
+        data: base64Data,
+        directory: Directory.Data
+    });
+
+    // Reload the file list
+    // Improve by only loading for the new image and unshifting array!
+    this.loadFiles();
+}
+
+  // https://ionicframework.com/docs/angular/your-first-app/3-saving-photos
+  private async readAsBase64(photo: Photo) {
+    if (this.plt.is('hybrid')) {
+        const file = await Filesystem.readFile({
+            path: photo.path!
+        });
+
+        return file.data;
+    }
+    else {
+        // Fetch the photo, read as a blob, then convert to base64 format
+        const response = await fetch(photo.webPath!);
+        const blob = await response.blob();
+
+        return await this.convertBlobToBase64(blob) as string;
+    }
+}
+
+// Helper function
+convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader;
+    reader.onerror = reject;
+    reader.onload = () => {
+        resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+});
+
+// Convert the base64 to blob data
+// and create  formData with it
+async startUpload(file: LocalFile) {
+  const response = await fetch(file.data);
+  const blob = await response.blob();
+  const formData = new FormData();
+  formData.append('file', blob, file.name);
+  this.uploadData(formData);
+}
+
+// Upload the formData to API
+async uploadData(formData: FormData) {
+  const loading = await this.loadingCtrl.create({
+      message: 'Uploading image...',
+  });
+  await loading.present();
+
+  // API
+  const url = 'http://localhost:8888/images/upload.php';
+
+  this.http.post(url, formData)
+      .pipe(
+          finalize(() => {
+              loading.dismiss();
+          })
+      )
+      .subscribe(res => {
+          if ((res as any)['success']) {
+              this.presentToast('File upload complete.')
+          } else {
+              this.presentToast('File upload failed.')
+          }
+      });
+}
+
+async deleteImage(file: LocalFile) {
+  await Filesystem.deleteFile({
+      directory: Directory.Data,
+      path: file.path
+  });
+  this.loadFiles();
+  this.presentToast('File removed.');
+}
 }
